4ae816b3ce5bc242ea589454598ea2e1628f8a3b
Brandi McCall <bm54cloud@gmail.com>
2023-02-01 13:52:50 +0000

    Feb1-2023

---
+++ AWSPythonResourceInventory.py
@@ -0,0 +1,8 @@
+# AWS Service Inventory
+# Create a list of services using Python. IE: S3, Lambda, EC2, etc
+# The list should be empty initially.
+# Populate the list using append or insert.
+# Print the list and the length of the list.
+# Remove two specific services from the list by name or by index.
+# Print the new list and the new length of the list.
+
