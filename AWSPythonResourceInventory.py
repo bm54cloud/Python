423a6baed967297ee4356986895ddb5675edd63e
Brandi McCall <bm54cloud@gmail.com>
2023-02-01 17:03:51 +0000

    update file

--- AWSPythonResourceInventory.py
+++ AWSPythonResourceInventory.py
@@ -1,8 +1,29 @@
-# AWS Service Inventory
-# Create a list of services using Python. IE: S3, Lambda, EC2, etc
+#!/usr/bin/env python3
+# Create a list of services using Python. IE: S3, Lambda, EC2, DynamoDB
+
 # The list should be empty initially.
+awspylist = []
+print(awspylist)
+
 # Populate the list using append or insert.
+awspylist.append('S3')
+awspylist.append('Lambda')
+awspylist.append('EC2')
+awspylist.append('DynamoDB')
+
 # Print the list and the length of the list.
+print(awspylist)
+print("The length of the list is", len(awspylist), "items")
+
 # Remove two specific services from the list by name or by index.
+
+
 # Print the new list and the new length of the list.
+
+
+
+    
+    
+
+
 
