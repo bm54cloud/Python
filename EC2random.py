47c480cf9b302872d8773ce287589aac9ad7927f
Brandi McCall <117415095+bm54cloud@users.noreply.github.com>
2023-02-04 11:48:52 -0700

    Update EC2random.py

--- EC2random.py
+++ EC2random.py
@@ -6,22 +6,26 @@
 # If a user puts another department, print a message that they should not use this Name Generator. 
 # Be sure to account for incorrect upper or lowercase letters in the correct department. 
 
-import random
+import random, string 
+
 instancenum = input("How many instances do you want to create names for? ")
 department = input("What is the name of your department (Marketing, Accounting, FinOps)? ")
 
 if department.lower() == 'marketing':
     print("Here are your unique EC2 instance names: ")
     for i in range(int(instancenum)):
-        print(department.capitalize(), random.randrange (9999999), sep="")
+        x = (department.capitalize() + ''.join(random.choices(string.ascii_letters + string.digits, k=16)))
+        print(x)
 elif department.lower() == 'accounting':
     print("Here are your unique EC2 instance names: ")
     for i in range(int(instancenum)):
-        print(department.capitalize(), random.randrange (9999999), sep="")
+        x = (department.capitalize() + ''.join(random.choices(string.ascii_letters + string.digits, k=16)))
+        print(x)
 elif department.lower() == 'finops':
     print("Here are your unique EC2 instance names: ")
     for i in range(int(instancenum)):
-        print('FinOps', random.randrange (9999999), sep="")
+        x = ('FinOps' + ''.join(random.choices(string.ascii_letters + string.digits, k=16)))
+        print(x)
 else:
     print("Your department is not authorized to use this Name Generator")
 
