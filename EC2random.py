285bff950556a14751c52ddeaa4a9b0e6d445841
Brandi McCall <117415095+bm54cloud@users.noreply.github.com>
2023-02-03 21:24:13 -0700

    Update EC2random.py

--- EC2random.py
+++ EC2random.py
@@ -1,15 +1,32 @@
-#!/usr/bin/env python3
-
-# Create an EC2 random name generator using random python library
-
-# Allow the user to input how many EC2 instances they want names for and output the same amount of unique names.
-
-# Allow the user to input the name of their department that is used in the unique name.
-
+# Allow user to input how many EC2 instances they want names for and output the same amount of unique names.
+# Allow user to input the name of their department that is used in the unique name.
 # Generate random characters and numbers that will be included in the unique name.
+# Only Marketing, Accounting, and FinOps departments should use this generator. 
+# List these departments as options 
+# If a user puts another department, print a message that they should not use this Name Generator. 
+# Be sure to account for incorrect upper or lowercase letters in the correct department. 
 
-# ADVANCED
-# The only departments that should use this Name Generator are the Marketing, Accounting, and FinOps Departments. List these departments as options and if a user puts another department, print a message that they should not use this Name Generator. Be sure to account for incorrect upper or lowercase letters in the correct department. Example: a user inputs accounting vs Accounting.
+instancenum = input("How many instances do you want to create names for? ")
+department = input("What is the name of your department? ")
 
-# COMPLEX
-# Turn the above into a Function and execute the Function to verify it works.
+if department.lower() == 'marketing':
+    print("Here are your unique EC2 instance names: ")
+    import random
+    for i in range(int(instancenum)):
+        print(department.capitalize(), random.randrange (9999999), sep="")
+elif department.lower() == 'accounting':
+    print("Here are your unique EC2 instance names: ")
+    import random
+    for i in range(int(instancenum)):
+        print(department.capitalize(), random.randrange (9999999), sep="")
+elif department.lower() == 'finops':
+    print("Here are your unique EC2 instance names: ")
+    import random
+    for i in range(int(instancenum)):
+        print('FinOps', random.randrange (9999999), sep="")
+else:
+    print("Your department is not authorized to use this Name Generator")
+
+
+
+
