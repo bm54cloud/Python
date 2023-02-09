b8d150cda3d9a57308cfa899865a843bdcf28775
Brandi McCall <bm54cloud@gmail.com>
2023-02-09 21:04:16 +0000

    feb 9 2023

---
+++ W13project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.7
+
+# Import OS module
+import os 
+
+# Build a script that extracts the name and size of a file in your current working directory
+path = os.getcwd()
+
+# Get list of all files in current working directory
+dir_list = list(os.listdir(path))
+
+# Create dictionary of all files in the current working directory
+file_dict = dict()
+
+# Get list of file sizes in current working directory
+# Dictionary syntax is dict_name[key] = value
+for something in dir_list:
+    file_size = os.stat(something)
+    file_dict[something] = file_size
+
+# {:d} tells the formatter to treat the argument as an integer
+# {}'.format(name)
+for item in file_dict:
+    print({"File: {:30s} Size: {:d} Bytes".format(item,file_dict[item].st_size)})
+
