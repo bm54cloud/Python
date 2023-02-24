e433e8f86b2c68413ceb496ce0b6c741e47bfee1
Brandi McCall <bm54cloud@gmail.com>
2023-02-24 21:26:58 +0000

    feb 24

---
+++ Python_bootstrap.py
@@ -0,0 +1,16 @@
+# Script to run Python on instance startup
+
+#!/bin/bash
+apt update -y
+apt install python3-pip -y
+apt install gunicorn -y
+apt install supervisor -y
+git clone https://github.com/zaireali649/python_cicd /home/ubuntu/python_cicd
+pip3 install -r /home/ubuntu/python_cicd/app/requirements.txt
+mkdir /etc/supervisor
+mkdir /etc/supervisor/conf.d
+cp /home/ubuntu/python_cicd/configs/flask_app.conf /etc/supervisor/conf.d/flask_app.conf
+mkdir /var/log/flask_app
+touch /var/log/flask_app/flask_app.out.log
+touch /var/log/flask_app/flask_app.err.log
+supervisorctl reload
